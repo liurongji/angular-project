942ab1a2ea689c7720c0eabb48294a2d44f0e6e7
刘荣基 <liurongji@chinafanda.com>
2021-02-05 12:10:30 +0800

    fix: table fix demo

---
+++ src/app/routes/dashboard/dashboard.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { STColumn, STComponent } from '@delon/abc/st';
+import { G2MiniBarData } from '@delon/chart';
+import { _HttpClient } from '@delon/theme';
+import { NzMessageService } from 'ng-zorro-antd/message';
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+})
+export class DashboardComponent implements OnInit {
+  ps = 20;
+  total = 200; // mock total
+  args = { _allow_anonymous: true, userid: null };
+  url = `https://api.randomuser.me/?results=20`;
+  events: G2MiniBarData[] = [];
+  scroll = { y: '230px' };
+
+  constructor(public http: _HttpClient, private message: NzMessageService) {}
+
+  ngOnInit(): void {
+    // this.http.get('/chart/visit').subscribe((res: G2MiniBarData[]) => (this.events = res.slice(0, 8)));
+  }
+
+  fullChange(val: boolean): void {
+    this.scroll = val ? { y: '350px' } : { y: '230px' };
+  }
+  params = { a: 1, b: 2 };
+  @ViewChild('st', { static: false }) private st: STComponent;
+  columns: STColumn[] = [
+    { title: 'id', index: 'id.value', type: 'checkbox' },
+    { title: 'Avatar', index: 'picture.thumbnail', type: 'img', width: 80 },
+    {
+      title: 'Name',
+      index: 'name.first',
+      width: 150,
+      format: (item) => `${item.name.first} ${item.name.last}`,
+      type: 'link',
+      click: (item) => this.message.info(`${item.name.first}`),
+    },
+    { title: 'Email', index: 'email' },
+    {
+      title: 'Gender',
+      index: 'gender',
+      type: 'yn',
+      yn: {
+        truth: 'female',
+        yes: '男',
+        no: '女',
+        mode: 'text',
+      },
+      width: 120,
+    },
+    { title: 'Events', render: 'events', width: 90 },
+    { title: 'Registered', index: 'registered.date', type: 'date', width: 170 },
+    {
+      title: 'Actions',
+      width: 120,
+      buttons: [
+        {
+          text: 'Edit',
+          click: (item) => this.message.info(`edit [${item.id.value}]`),
+          iif: (item) => item.gender === 'female',
+        },
+        {
+          text: 'Delete',
+          type: 'del',
+          click: (item) => this.message.info(`deleted [${item.id.value}]`),
+        },
+      ],
+    },
+  ];
+
+  setRow(): void {
+    this.st.setRow(0, { price: 100000000 });
+  }
+}
